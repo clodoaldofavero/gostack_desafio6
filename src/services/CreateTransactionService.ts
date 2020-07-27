5fb550469c8a037affbce2c50a4268930ba32f1f
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2020-07-27 00:10:02 -0300

    Cria repository para Categories e implementa Execute no CreateTransactionService.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
+import { getCustomRepository } from 'typeorm';
 import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+
+interface Request {
+  value: number;
+  type: 'income' | 'outcome';
+  category: 'string';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getCustomRepository(CategoriesRepository);
+
+    const balance = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new AppError('Invalid balance');
+    }
+
+    const foundCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    let category_id = '0';
+    if (!foundCategory) {
+      const categoryNew = categoryRepository.create({ title: category });
+      await categoryRepository.save(categoryNew);
+
+      category_id = categoryNew.id;
+    } else {
+      category_id = foundCategory.id;
+    }
+
+    const transaction = transactionRepository.create({
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
